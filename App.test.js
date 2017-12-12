dde04c1f4ebe55ab575ceaf97c31f6aca1587d59
mitchpollock <mitchell.g.pollock@vanderbilt.edu>
2017-12-11 19:20:05 -0600

    added enzyme to test

--- App.test.js
+++ App.test.js
@@ -1,10 +1,15 @@
-import 'react-native';
+
 import React from 'react';
 import App from './App';
+import {shallow} from 'enzyme'
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+configure({ adapter: new Adapter() });
 
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toMatchSnapshot();
+  const wrapper = shallow(<App />);
+  expect(app).toMatchSnapshot();
 });
